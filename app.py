2ec0d75b8cdd204030b6888d4d3ac821b2a4bd32
atkinsonclaire <78178214+atkinsonclaire@users.noreply.github.com>
2021-06-02 19:39:40 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import flask, jsonify
-app=Flask(_name_)
+from flask import Flask, jsonify
+import sqlalchemy
+from sqlalchemy.orm import Session
+
+app = Flask(__name__)
 
 @app.route("/")
 def welcome():
@@ -15,13 +18,12 @@ def welcome():
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     session = Session(engine)
-
-    results = session.query(Precipitation.date, Precipitation.prcp).all()
+    precip_results = session.query(Precipitation.date, Precipitation.prcp).all()
 
     session.close()
 
     all_precip = []
-    for date, prcp in results:
+    for date, prcp in precip_results:
         precip_dict = {}
         precip_dict["date"] = date
         precip_dict["prcp"] = prcp
@@ -29,23 +31,21 @@ def precipitation():
 
     return jsonify(all_precip)
 
-@app.route("/api/v1.0/stations)
+@app.route("/api/v1.0/stations")
 def stations():
     session = Session(engine)
 
-    results = session.query(Stations.stations).all()
+    stations_results = session.query(Stations.stations).all()
 
     session.close()
 
-    all_stations = list(np.ravel(results))
+    return jsonify(stations_results)
 
-    return jsonify(all_stations)
-
-@app.route("/api/v1.0/tobs)
+@app.route("/api/v1.0/tobs")
 def tobs():
     session = Session(engine)
 
-    results = session.query(Measurement.tobs, Measurement.date).\
+    tobs_results = session.query(Measurement.tobs, Measurement.date).\
         filter(Measurement.station == 'USC00519281').\
         filter(Measurement.date < '2017-08-18').\
         filter(Measurement.date > '2016-08-18').\
@@ -53,28 +53,43 @@ def tobs():
 
     session.close()
 
-    all_tobs = list(np.ravel(results))
+    return jsonify(tobs_results)
 
-    return jsonify(all_tobs)
+@app.route("/api/v1.0/<start>")
+def start(start_date):
+    session=Session(engine)
+    start_date_results=session.query(Measurement.date, Measurement.tobs)
 
-@app.route("/api/v1.0/<start>)
-def start():
-    session = Session(engine)
-
-    min_temp=session.query(func.min(Measurement.tobs)).\
-        filter(Measurement.station == 'USC00519281')
-    max_temp=session.query(func.max(Measurement.tobs)).\
-        filter(Measurement.station == 'USC00519281')
-    avg_temp=session.query(func.avg(Measurement.tobs)).\
-        filter(Measurement.station == 'USC00519281')
+    for start in start_date_results:
+        TMIN=session.query(func.min(Measurement.tobs)).\
+            filter(Measurement.date >= start)
+        TMAX=session.query(func.max(Measurement.tobs)).\
+            filter(Measurement.date >= start)
+        TAVG=session.query(func.avg(Measurement.tobs)).\
+            filter(Measurement.station >= start)
     session.close()
+        
+    return jsonify({"Min Temp": {TMIN}, "Max Temp:": {TMAX}, "Avg Temp": {TAVG} })
 
-    all_min = list(np.ravel(min_temp))
-    return jsonify(all_min)
-    all_max = list(np.ravel(max_temp))
-    return jsonify(all_max)
-    all_avg = list(np.ravel(avg_temp))
-    return jsonify(all_avg)
+@app.route("/api/v1.0/<start>/<end>")
+def end(start_date, end_date):
+    session=Session(engine)
+    date_results=session.query(Measurement.date, Measurement.tobs)
 
-if __name__ == '__main__':
+    for start, end in date_results:
+        TMIN=session.query(func.min(Measurement.tobs)).\
+            filter(Measurement.date >= start).\
+            filter(Measurement.date <= end)
+        TMAX=session.query(func.max(Measurement.tobs)).\
+            filter(Measurement.date >= start).\
+            filter(Measurement.date <= end)
+        TAVG=session.query(func.avg(Measurement.tobs)).\
+            filter(Measurement.date >= start).\
+            filter(Measurement.date <= end)
+    session.close()
+        
+    return jsonify({"Min Temp": {TMIN}, "Max Temp:": {TMAX}, "Avg Temp": {TAVG} })
+
+
+if __name__ == "__main__":
     app.run(debug=True)
